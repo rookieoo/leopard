b2b5fea63cfe697eb060755d54c7e6ea9bc3c4db
solomonrookie <77053375+rookieoo@users.noreply.github.com>
2022-12-12 02:22:58 +0800

    更改应用模型

--- horse/models.py
+++ horse/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Work(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    description = models.CharField(max_length=250)
